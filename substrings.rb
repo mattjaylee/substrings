4b848cf5d4926c381f93612a5a2242ca585117c9
Matt Lee <matt.jay.lee@gmail.com>
2022-07-30 12:42:12 -0700

    Add support for multiple input words

--- substrings.rb
+++ substrings.rb
@@ -2,17 +2,20 @@ require 'pry-byebug'
 
 def substrings(string, dictionary)
     substring_hash = {}
-    string_input_array = string.split('')
-    string_input_array.each_with_index do |source_char, source_index|
-        for word in dictionary do
-            running_index = source_index
-            dict_word_array = word.split('')
-            dict_word_array.each_with_index do |dict_char, dict_index|
-                if dict_word_array.length - 1 == dict_index && dict_word_array[dict_index] == string_input_array[running_index]
-                    substring_hash.has_key?(word) ? substring_hash[word] += 1 : substring_hash[word] = 1
-                elsif dict_word_array[dict_index] == string_input_array[running_index]
-                    running_index += 1
-                else break
+    separated_strings = string.split(/[^[[:word:]]]+/)
+    separated_strings.each do |sep_string|
+        string_input_array = sep_string.downcase.split('')
+        string_input_array.each_with_index do |source_char, source_index|
+            for word in dictionary do
+                running_index = source_index
+                dict_word_array = word.split('')
+                dict_word_array.each_with_index do |dict_char, dict_index|
+                    if dict_word_array.length - 1 == dict_index && dict_word_array[dict_index] == string_input_array[running_index]
+                        substring_hash.has_key?(word) ? substring_hash[word] += 1 : substring_hash[word] = 1
+                    elsif dict_word_array[dict_index] == string_input_array[running_index]
+                        running_index += 1
+                    else break
+                    end
                 end
             end
         end
@@ -20,5 +23,5 @@ def substrings(string, dictionary)
     puts substring_hash
 end
 
-user_dictionary = ['below', 'be', 'no', 'low', 'nope', 'be']
-substrings('below', user_dictionary)
+user_dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+substrings("Howdy partner, sit down! How's it going?", user_dictionary)
